6843526f431b0a4278c73e6644f35a76a7509ef2
Hitesh <gaikwadhitesh03@gmail.com>
2023-12-09 23:48:43 +0530

    cors resolved

--- src/main/java/com/coder/movies/MovieController.java
+++ src/main/java/com/coder/movies/MovieController.java
@@ -4,14 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
@@ -19,11 +17,13 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
     @GetMapping
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
     }
 
     @GetMapping("/{imdbId}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId){
         Optional<Movie> movie= movieService.getMovie(imdbId);
         return new ResponseEntity<Optional<Movie>>(movie,HttpStatus.OK);
--- src/main/java/com/coder/movies/ReviewController.java
+++ src/main/java/com/coder/movies/ReviewController.java
@@ -3,13 +3,11 @@ package com.coder.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
@@ -18,6 +16,7 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")), HttpStatus.CREATED);
     }
